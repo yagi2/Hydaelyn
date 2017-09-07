151b47666c2b33551a816017bd07aa614ed6bad1
Itsuki Aoyagi <mail@yagi2.com>
2017-09-07 10:35:07 +0900

    init from start.spring.io with web, devtools and JPA

---
+++ src/main/kotlin/com/yagi2/hydaelyn/HydaelynApplication.kt
@@ -0,0 +1,11 @@
+package com.yagi2.hydaelyn
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HydaelynApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(HydaelynApplication::class.java, *args)
+}
